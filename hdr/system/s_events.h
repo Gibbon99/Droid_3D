4c2750ef83fb5cd9e97932c626647e411b699180
Gibbon99 <dberry@xenon.network.home>
2018-06-18 20:43:24 +1000

    Commit events.h

---
+++ hdr/system/s_events.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define USER_EVENT_MOUSE_BUTTON_DOWN    0x0
+#define USER_EVENT_MOUSE_BUTTON_UP      0x1
+#define USER_EVENT_MODE_PAUSE           0x2
+
+// Main event routine - handle all the events coming in and
+// farm them to the correct routine
+void evt_handleEvents();
+
+// Setup user event source and event data
+bool evt_registerUserEventSetup();
+
+// Create a custom event to be sent
+void evt_sendEvent(int eventType, int eventData1);
